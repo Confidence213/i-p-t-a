9e77d6158813275aa7cfdc0879b7cb09c4427218
jackcolin21307 <jackcolin21307@gmail.com>
2022-08-07 04:55:38 +0300

    мелкие правки

--- src/constants.ts
+++ src/constants.ts
@@ -11,19 +11,22 @@ export const FILTERS: FilterItem[] = [
     id: "0-transfers",
     value: 0,
     label: "Без пересадок"
-  },{
+  },
+  {
     id: "1-transfer",
     value: 1,
     label: "1 пересадка"
-  },{
+  },
+  {
     id: "2-transfers",
     value: 2,
     label: "2 пересадки"
-  },{
+  },
+  {
     id: "3-transfers",
     value: 3,
     label: "3 пересадки"
-  },
+  }
 ];
 
 export const FILTER_MAP: { [key: string]: string } = {
